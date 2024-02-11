4e064c744a5a862c9828ad00c2ba06ec6c833661
TomBCodes <thomas.bedford89@gmail.com>
2024-02-12 01:00:10 +0800

    Make planning notes; move old maze components into a test folder

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,60 @@
 import React from 'react'
-import Maze from '../components/Maze.jsx'
+import MazeTest from './mazetest/MazeTest.jsx'
+import Maze from './maze/Maze.jsx'
+
+
+const MazeContext = React.createContext()
+
 
 export default function Main() {
+
+    // WRAP MAIN WITH CONTEXT FOR ALL OPTIONS WHICH WILL IMPACT THE MAZEGEN / CLICKING / PATHFINDING
+    //      THIS MEANS MAZE ARRAY HAS TO BE HERE
+
+    // I'D LIKE MOST OF THE SCREEN TO BE THE GRID, WITH AN OPTIONS MENU TO SLIDE FROM THE RIGHT
+    //      (WITH SOME BASIC OPTIONS ALWAYS VISIBLE?)
+
+    // OPTIONS:
+    //      CLICKING:
+    //          WALL; PATH; FOREST; MOUNTAIN; START; END (get react-icons for start/end)
+    //      WHICH MAZEGEN ALGO:
+    //          KRUSKALS, PRIMMS, BACKTRACKING, HUNT AND KILL
+    //      WHICH PATHFINDING ALGO:
+    //          BFS, DFS (TURN LEFT), DIJKSTRA'S, A-STAR
+    //      TOGGLES:
+    //          WALLS UNEDITABLE
+    //          SHOW GENERATION/PATHFINDING (SLOW-MO)
+    // STRETCH:
+    //      RESIZE MAZE
+
+
+
+    const [mazeArr, setMazeArr] = React.useState([
+        // CREATE MAZE ARRAY OF OBJECTS HERE
+    ])
+
+    function updateMaze() {
+        // const newArr = mazeArr.map(arr => arr.slice())
+        // doesn't solve issue cos objects wont be deep copied
+    }
+
+
+
     return (
-        <main>
-            <div className='main--maze-container'>
-                <h2>Maze</h2>
-                <Maze />
-            </div>
-            <div className='main--options-container'>
-                <h2>Options</h2>
-            </div>
-        </main>
+        <MazeContext.Provider value={{
+            // ADD IN ALL CONTEXT VALUES HERE
+        }}>
+            <main>
+                <div className='main--maze-container'>
+                    <h2>Maze</h2>
+                    {/* <MazeTest /> */}
+                    <Maze />
+                </div>
+                <div className='main--options-container'>
+                    <h2>Options</h2>
+                    <div className='options--container'></div>
+                </div>
+            </main>
+        </MazeContext.Provider>
     )
 }
---
+++ src/components/maze/Maze.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Maze() {
+
+    return (
+        <div className='maze--container'>
+            <h3>Real maze to go here</h3>
+        </div>
+    )
+}
+
+
